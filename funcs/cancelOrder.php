6425451387cad1597c6c65a710934ffcd7bee11d
ILDARON123 <123239094+ILDARON123@users.noreply.github.com>
2023-06-29 11:28:25 +0500

    234234242342342342

--- funcs/cancelOrder.php
+++ funcs/cancelOrder.php
@@ -1,10 +1,17 @@
 <?php
+require("user.php");
 require("../admin/security.php");
-require("setProductDB.php");
+require("DBinteraction.php");
 if (isset($_GET['reason']) && isset($_SESSION['maydel'])) {
-$query = "INSERT INTO `cancelled_orders` (`id`, `reason`, `orders`) VALUES (NULL,'".$_GET['reason']."','".$_SESSION['mayDel']."')";
-$_SESSION['result'] = "Отказано.";
+    $query = "INSERT INTO `cancelled_orders` (`id`, `reason`, `order`) VALUES (NULL,'" . $_GET['reason'] . "','" . $_SESSION['mayDel'] . "')";
+    insertOrUpdate($query);
+
+    $query = "UPDATE `orders` SET `status`='3' WHERE `id` = " . $_SESSION['mayDel'];
+    insertOrUpdate($query);
+
+    $_SESSION['result'] = "Отказано.";
 } else {
     $_SESSION['result'] = "Недостаточно информации.";
 }
-header("location: /admin.php");
+echo $_SESSION['result'];
+// header("location: /admin.php");
